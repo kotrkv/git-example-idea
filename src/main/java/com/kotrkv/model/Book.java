9a9955b9a75cee64c3cc4a8f2daa19af20780106
konstantin <kotrkv@gmail.com>
2022-02-19 17:34:30 +0300

    Добавил модель Book

---
+++ src/main/java/com/kotrkv/model/Book.java
@@ -0,0 +1,11 @@
+package com.kotrkv.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+    private Long id;
+    private String author;
+}

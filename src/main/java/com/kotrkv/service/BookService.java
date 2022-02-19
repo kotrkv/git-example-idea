034c083ad4748c7e27958cb0fd4b1d7eb000cecd
konstantin <kotrkv@gmail.com>
2022-02-19 17:51:17 +0300

    Изменил тип возвращаемого значения BookService

--- src/main/java/com/kotrkv/service/BookService.java
+++ src/main/java/com/kotrkv/service/BookService.java
@@ -2,12 +2,13 @@ package com.kotrkv.service;
 
 import com.kotrkv.model.Book;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Service
 public class BookService {
-    public List<Book> books() {
-        return List.of(new Book(1L, "Букварь"));
+    public Mono<List<Book>> books() {
+        return Mono.just(List.of(new Book(1L, "Букварь")));
     }
 }
